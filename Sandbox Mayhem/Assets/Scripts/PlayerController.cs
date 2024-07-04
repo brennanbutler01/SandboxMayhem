a9e2ff5030d07bd7447ac6a29044f631502665b9
Chris Mayers <cmayers7@gatech.edu>
2024-07-04 09:33:51 -0700

    Dynamically show/hide coins and text based on coin boost active state

--- Sandbox Mayhem/Assets/Scripts/PlayerController.cs
+++ Sandbox Mayhem/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,9 @@ public class PlayerController : MonoBehaviour
     private int _trailingWaypoint;
     public bool isGoingBackward, isHalfway, isHuman, lapPenalty, hasFinished;
     private GameObject[] uiCoins;
+    private GameObject coinBoostActiveText;
+    private bool alreadyBoosting = false;
+    private bool allCoinsShowing = false;
     private void Start()
     {
         coins = 0;
@@ -27,8 +30,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.LogError("PlayerController requires a car controller");
         }
-        uiCoins = GameObject.FindGameObjectsWithTag("UICoin");
-
+        uiCoins = GameObject.FindGameObjectsWithTag("UICoin").OrderBy(x => x.name).ToArray();
+        coinBoostActiveText = GameObject.FindGameObjectWithTag("CoinBoostActiveText");
     }
 
     public float GetDistanceToNextWaypoint(int index)
@@ -61,20 +64,41 @@ public class PlayerController : MonoBehaviour
         {
             isGoingBackward = true;
         }
-        if (isHuman)
+
+        // Show/hide coins in UI depending on each coin collected
+        // Only run if collector is human and all coins are not already showing
+        if (isHuman && !allCoinsShowing)
         {
-            for (int i = 0; i < 5; i++)
+            coinBoostActiveText.SetActive(false);
+            if (coins % 5 != 0 || coins == 0)
             {
-                if (i < (coins % 5))
+                alreadyBoosting = false;
+                for (int i = 0; i < 5; i++)
+                {
+                    if (i < (coins % 5))
+                    {
+                        uiCoins[i].SetActive(true);
+                    }
+                    else
+                    {
+                        uiCoins[i].SetActive(false);
+                    }
+
+                }
+            }
+            // If player has 5 coins and isn't in a coin boost state
+            // Show all coins and coin boost text and enter a coin boost state
+            else if (!alreadyBoosting)
+            {
+                allCoinsShowing = true;
+                coinBoostActiveText.SetActive(true);
+                for (int i = 0; i < 5; i++)
                 {
                     uiCoins[i].SetActive(true);
                 }
-                else
-                {
-                    uiCoins[i].SetActive(false);
-                }
-            
-            }
+                StartCoroutine(_hideUICoins());
+                alreadyBoosting = true;
+            }                    
         }
     }
 
@@ -83,5 +107,19 @@ public class PlayerController : MonoBehaviour
         // after a second, we will set our trailing wp
         yield return new WaitForSeconds(1);
         _trailingWaypoint = lastWaypoint;
+    }
+
+    // Show coins for 3 seconds, the duration of the coin boost
+    // At the end, leave the coin boost state and hide coin boost text
+    private IEnumerator _hideUICoins()
+    {
+        yield return new WaitForSeconds(3);
+        for (int i = 0; i < 5; i++)
+        {
+            uiCoins[i].SetActive(false);
+        }
+        StopCoroutine(_hideUICoins());
+        allCoinsShowing = false;
+        coinBoostActiveText.SetActive(false);
     }
 }
