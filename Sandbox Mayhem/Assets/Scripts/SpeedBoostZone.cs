a1bd15c9e5371d81da74c88fdad77a29a03c1496
Butler, Brennan Asa <bbutler46@gatech.edu>
2024-07-04 17:06:24 -0700

    Boost zone replacement (#62)
    * added better appearance for speed boost zones
    
    * updated track
    
    * got rid of two boost zones

--- Sandbox Mayhem/Assets/Scripts/SpeedBoostZone.cs
+++ Sandbox Mayhem/Assets/Scripts/SpeedBoostZone.cs
@@ -7,21 +7,43 @@ public class SpeedBoostZone : MonoBehaviour
     private float _boostMultiplier = 2f;
     private const float MaxBoostMultiplier = 5f;
     private const float BoostIncreaseRate = .5f;
+    public float height = 5f; // Height of the vertical plane
+    public float scrollSpeed = 1f;
+
+    private Material _material;
+
+    private void Start()
+    {
+        var component = GetComponent<Renderer>();
+        _material = component.material;
+
+        var boxCollider = GetComponent<BoxCollider>();
+        if (boxCollider == null) return;
+        
+        boxCollider.size = new Vector3(transform.localScale.x, height, 0.1f);
+        boxCollider.center = new Vector3(0, height / 2, 0);
+    }
+
+    private void Update()
+    {
+        // Scroll the texture
+        var offset = Time.time * scrollSpeed;
+        _material.mainTextureOffset = new Vector2(-offset, 0);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         var carController = other.GetComponent<CarController>();
         if (carController == null) return;
 
-        if (other.CompareTag("Player"))
+        if (!other.CompareTag("Player")) return;
+        var player = other.GetComponent<PlayerController>();
+        Debug.Log("Boost zoning...");
+        if (player.isHuman)
         {
-            var player = other.GetComponent<PlayerController>();
-            Debug.Log("Boost zoning...");
-            if (player.isHuman)
-            {
-                EventManager.TriggerEvent<SpeedBoostZoneEvent, GameObject>(other.gameObject);
-            }
-            carController.ApplyBoostZone(_boostMultiplier);
+            EventManager.TriggerEvent<SpeedBoostZoneEvent, GameObject>(other.gameObject);
         }
+        carController.ApplyBoostZone(_boostMultiplier);
     }
 
     private void OnTriggerStay(Collider other)
@@ -29,14 +51,11 @@ public class SpeedBoostZone : MonoBehaviour
         var carController = other.GetComponent<CarController>();
         if (carController == null) return;
 
-        if (other.CompareTag("Player"))
-        {
-            // increase over time to the maximum
-            _boostMultiplier = Mathf.Min(_boostMultiplier + BoostIncreaseRate * Time.deltaTime, MaxBoostMultiplier);
-            carController.ApplyBoostZone(_boostMultiplier);
-            Debug.Log("Increasing the boost");
-            
-        }
+        if (!other.CompareTag("Player")) return;
+        // increase over time to the maximum
+        _boostMultiplier = Mathf.Min(_boostMultiplier + BoostIncreaseRate * Time.deltaTime, MaxBoostMultiplier);
+        carController.ApplyBoostZone(_boostMultiplier);
+        Debug.Log("Increasing the boost");
     }
     
     private IEnumerator _reset(CarController carController)
