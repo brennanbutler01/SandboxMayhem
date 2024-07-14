0ab39f91a9424c4f6d2c7e71aa11523d2f3c94bb
Chris Mayers <cmayers7@gatech.edu>
2024-07-14 12:10:18 -0700

    Merge

--- Sandbox Mayhem/Assets/Scripts/SpeedBoostZone.cs
+++ Sandbox Mayhem/Assets/Scripts/SpeedBoostZone.cs
@@ -49,9 +49,9 @@ public class SpeedBoostZone : MonoBehaviour
         _boostMultiplier = Mathf.Min(_boostMultiplier + BoostIncreaseRate * Time.deltaTime, MaxBoostMultiplier);
         if (carController.GetComponent<PlayerController>().isHuman)
         {
-
-        carController.ApplyBoostZone(_boostMultiplier);
-        // Debug.Log("Increasing the boost");
+            carController.ApplyBoostZone(_boostMultiplier);
+        }
+        Debug.Log("Increasing the boost");
     }
     
     private IEnumerator _reset(CarController carController)
@@ -68,7 +68,7 @@ public class SpeedBoostZone : MonoBehaviour
 
         if (!other.CompareTag("Player")) return;
         
-        // Debug.Log("Leaving zone...");
+        Debug.Log("Leaving zone...");
         StartCoroutine(_reset(carController));
     }
 }
