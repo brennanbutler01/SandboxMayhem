f6a1c1a5222f838939e1154460c775bc849d220a
Runolfson, Bjarni <brunolfson3@gatech.edu>
2024-07-19 12:56:45 -0700

    BR Polish Update (#107)
    * Updated Main Menu
    
    * Temp New Main Menu
    
    * Updated Menu
    
    * Removed extra scene, adjusted scenery, removed end race key
    
    * Removed end race key
    
    * Removed extra scene (for real this time I hope)

--- Sandbox Mayhem/Assets/Scripts/MainMenu.cs
+++ Sandbox Mayhem/Assets/Scripts/MainMenu.cs
@@ -1,26 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Xml;
 
 public class MainMenu : MonoBehaviour
 {
     public string firstLevel;
     public GameObject optionsScreen;
+    public GameObject mainMenuUI;
+    public GameObject storyCanvas;
     public GameObject creditsScreen;
     public GameObject firstIntroScreen;
 
     // Start is called before the first frame update
     void Start()
     {
+        mainMenuUI.SetActive(true);
         optionsScreen.SetActive(false);
+        storyCanvas.SetActive(false);
+        creditsScreen.SetActive(false);
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(firstLevel);
         CloseCredits();
     }
 
     public void StartGame()
     {
-        //SceneManager.LoadScene(firstLevel);
+
+        storyCanvas.SetActive(true);
+        mainMenuUI.SetActive(false);
         firstIntroScreen.SetActive(true);
+        
+
     }
 
     public void OpenOptions()
